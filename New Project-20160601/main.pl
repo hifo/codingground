bdb1192c36aedec7361b6812208eefcc6c5101d6
Hillary <casscainbatgirl@gmail.com>
2016-06-01 19:33:28 -0400

    get_char_trait()

--- New Project-20160601/main.pl
+++ New Project-20160601/main.pl
@@ -1,40 +1,56 @@
 #
 # Hello World Program in Perl
 #
-use Switch;
-
 $exit = 0;
-%chars = ('Fang' => ["fighter", "canid", "quadraped"],
-            'Joey' => ["canid", "quadraped"],
-            'Bridget' => ["intelligent", "time magic"],
-            'Tyaxha' => ["intelligent", "fighter"],
-            'Dani' => ["time magic"]
+
+%characters = (fang => ["fighter", "canid", "quadraped"],
+	       joey => ["canid", "quadraped"],
+	       bridget => ["intelligent", "time magic"],
+	       tyaxha => ["intelligent", "fighter"],
+	       dani => ["time magic"]
         );
         
-%commands = (1 => "get_names",
-                2 => "exit"    
+%commands = (0 => "0. Exit Program \r\n",
+	     1 => "1. Get all characters \r\n",
+	     2 => "2. Get character traits \r\n"
             );
             
-@names = keys %chars;
+@names = keys %characters;
 
 sub get_names{
     foreach(@names){
         print "$_\r\n";
     }
+    print "\r\n";
 }   
+
+sub get_char_traits{
+    print "Which character?\r\n";
+    print ": ";
+    $char = <>;
+    chomp($char);
+    $char = lc $char;
+    print "Printing traits for $char\r\n";
+    print "@{$characters{$char}}";
+    print "\r\n";
+}
 
 while(!$exit){
     print "The following commands are available\r\n";
-    print "1. Get All Characters\r\n";
-    print "2. Exit Program \r\n";
+    @commandnames = keys %commands;
+    for $command (@commandnames){ 
+	print $commands{$command};
+    }
     print "Select a command to execute by entering it's number: ";
     $command = <>;
-    switch($command){
-        case 1 {
-            get_names();   
-        }
-        case 2 {
-           $exit = 1; 
-        }
+    chomp($command);
+    if($command == 1){
+	get_names();
+    }
+    elsif($command == 0){
+	$exit = 1;
+    }
+    elsif($command == 2){
+	get_char_traits();
     }
 }
